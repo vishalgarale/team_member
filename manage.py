69d14d031904d822474b606237e2a38c2a645be9
Vishal Garale <vishal@datawrkz.com>
2019-11-04 15:54:00 +0530

    Environments created

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_member_management.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_member_management.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ team_member_management/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['0.0.0.0', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
